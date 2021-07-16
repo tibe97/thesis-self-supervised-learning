7a82947715eb5b1a279189a652a5ded233e7ae6c
Tiberio <tiberio-97@hotmail.it>
2021-07-16 23:22:43 +0200

    fix

--- lightly/linear_evaluation/transforms.py
+++ lightly/linear_evaluation/transforms.py
@@ -5,6 +5,8 @@ import numpy as np
 from lightly.linear_evaluation.pl_bolts.utils import _OPENCV_AVAILABLE, _TORCHVISION_AVAILABLE
 from lightly.linear_evaluation.pl_bolts.utils.warnings import warn_missing_pkg
 
+
+'''
 if _TORCHVISION_AVAILABLE:
     from torchvision import transforms as transforms
 else:  # pragma: no cover
@@ -14,7 +16,7 @@ if _OPENCV_AVAILABLE:
     import cv2
 else:  # pragma: no cover
     warn_missing_pkg('cv2', pypi_name='opencv-python')
-
+'''
 
 class SwAVTrainDataTransform(object):
 
