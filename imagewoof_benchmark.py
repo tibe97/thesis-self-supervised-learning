90294f31cad1961b9c75a5d2ebddb8d54d132496
Tiberio <tiberio-97@hotmail.it>
2021-07-18 12:15:20 +0200

    fix

--- imagewoof_benchmark.py
+++ imagewoof_benchmark.py
@@ -183,7 +183,7 @@ dataset_train_ssl = lightly.data.LightlyDataset.from_torch_dataset(ssl_train_dat
 # we use test transformations for getting the feature for kNN on train data
 dataset_train_kNN = lightly.data.LightlyDataset.from_torch_dataset(copy.deepcopy(train_dataset.dataset), transform=test_transforms)
 
-valid_dataset.trasform = torchvision.transforms.Compose([
+valid_dataset.dataset.trasform = torchvision.transforms.Compose([
     torchvision.transforms.ToTensor()
 ])
 dataset_test = lightly.data.LightlyDataset.from_torch_dataset(copy.deepcopy(valid_dataset.dataset), transform=test_transforms)
