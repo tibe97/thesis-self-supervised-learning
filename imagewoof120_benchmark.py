b744bac291ca92338c6fbdf0b351132728f5884b
Tiberio <tiberio-97@hotmail.it>
2021-08-07 13:18:19 +0200

    run MoCo

--- imagewoof120_benchmark.py
+++ imagewoof120_benchmark.py
@@ -87,7 +87,7 @@ from lightly.models.modules.my_nn_memory_bank import MyNNMemoryBankModule
 from lightly.loss.my_ntx_ent_loss import MyNTXentLoss
 from pytorch_lightning.loggers import WandbLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
-from benchmark_models import NNCLR_HardNegative, BYOLModel, NNCLRModel, NNNModel, SimCLRModel, SimSiamModel, BarlowTwinsModel,NNBYOLModel
+from benchmark_models import NNCLR_HardNegative, BYOLModel, NNCLRModel, NNNModel, SimCLRModel, SimSiamModel, BarlowTwinsModel,NNBYOLModel,MocoModel
 
 num_workers = 12
 memory_bank_size = 4096
@@ -217,8 +217,8 @@ model_names = ['MoCo_256', 'SimCLR_256', 'SimSiam_256', 'BarlowTwins_256',
 models = [MocoModel, SimCLRModel, SimSiamModel, BarlowTwinsModel, 
           BYOLModel, NNCLRModel, NNSimSiamModel, NNBYOLModel]
 """
-model_names = ["NNN"]
-models = [NNNModel]
+model_names = ["MoCo_256"]
+models = [MocoModel]
 
 
 bench_results = []
