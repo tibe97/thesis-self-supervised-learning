374afd16bbc1d6e9f6cc220a77eae3bb2da51415
Tiberio <tiberio-97@hotmail.it>
2021-11-09 13:09:01 +0100

    nnn

--- stl10_benchmark.py
+++ stl10_benchmark.py
@@ -190,8 +190,8 @@ models = [MocoModel, SimCLRModel, SimSiamModel, BarlowTwinsModel,
           BYOLModel, NNCLRModel, NNSimSiamModel, NNBYOLModel]
 """
 
-model_names = ["NNN_Neg"]
-models = [NNNModel_Neg]
+model_names = ["NNN"]
+models = [NNNModel]
 
 
 bench_results = []
