added prototypes normalization plus swav loss fix

lightly/loss/my_ntx_ent_loss.py
@@ -154,8 +154,8 @@ class MyNTXentLoss(MemoryBankModule):
         loss = self.cross_entropy(logits, labels)
         if self.add_swav_loss: 
             p1 = self.softmax(q1 / self.temperature)
-            ipdb.set_trace()
-            loss = 0.5 * (loss - torch.mean(torch.sum(q0_assign * torch.log(p1), dim=1))) 
+            #ipdb.set_trace()
+            loss = (loss - torch.mean(torch.sum(q0_assign * torch.log(p1), dim=1))) 
 
         return loss
 

lightly/models/modules/my_nn_memory_bank.py
@@ -74,11 +74,11 @@ class MyNNMemoryBankModule(MemoryBankModule):
         #compute cluster assignments
         #start_time = time.time()
         with torch.no_grad():
-            """
+            # normalize prototypes
             w = self.model.prototypes_layer.weight.data.clone()
             w = torch.nn.functional.normalize(w, dim=1, p=2)
             self.model.prototypes_layer.weight.copy_(w)
-            """
+            
             cluster_scores = torch.mm(torch.cat((output_normed, bank_normed)), self.model.prototypes_layer.weight.t())
 
             q = torch.exp(cluster_scores / self.epsilon).t()