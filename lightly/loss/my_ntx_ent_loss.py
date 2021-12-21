0a21bd0840ba3842b31c21be6b9d5f55543d4dfd
Tiberio <tiberio-97@hotmail.it>
2021-12-21 15:29:42 +0100

    swav training

--- lightly/loss/my_ntx_ent_loss.py
+++ lightly/loss/my_ntx_ent_loss.py
@@ -158,10 +158,11 @@ class MyNTXentLoss(MemoryBankModule):
         swav_loss = None
         
         alpha = torch.tensor(1.0) # swav_loss influence on the overall loss
-        if self.add_swav_loss and negatives is not None: 
+        if self.add_swav_loss: # and negatives is not None: 
             p1 = self.softmax(q1 / self.temperature)
             swav_loss = - torch.mean(torch.sum(q0_assign * torch.log(p1), dim=1))
-            loss = (contrastive_loss + alpha * swav_loss) 
+            #loss = (contrastive_loss + alpha * swav_loss) 
+            loss = swav_loss
 
         return loss, swav_loss, contrastive_loss
 
--- stl10_benchmark.py
+++ stl10_benchmark.py
@@ -58,7 +58,7 @@ memory_bank_size = 4096
 
 my_nn_memory_bank_size = 2048
 temperature=0.5
-warmup_epochs=0
+warmup_epochs=400
 nmb_prototypes=100
 num_negatives=256
 use_sinkhorn = True
