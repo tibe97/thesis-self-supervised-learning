d15153e0f892e999f2271b8f1a1d05b1fd42e61a
Tiberio <tiberio-97@hotmail.it>
2022-03-11 12:25:51 +0100

    fix

--- stl10_visualize.py
+++ stl10_visualize.py
@@ -27,7 +27,7 @@ from benchmark_models import MocoModel, BYOLModel, NNCLRModel, NNNModel, SimCLRM
 #import tensorflow as tf
 from tensorboard.plugins import projector
 
-checkpoint_path = "checkpoint/STL10/epoch=399-step=155999.ckpt"
+checkpoint_path = "checkpoints/STL10/epoch=399-step=155999.ckpt"
 num_workers = 2
 memory_bank_size = 4096
 
