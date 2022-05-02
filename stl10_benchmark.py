c5ef0a1875ceb932271ebd345d28019967e89472
Tiberio <tiberio-97@hotmail.it>
2022-05-02 23:03:18 +0200

    run simCLR

--- stl10_benchmark.py
+++ stl10_benchmark.py
@@ -180,8 +180,8 @@ models = [MocoModel, SimCLRModel, SimSiamModel, BarlowTwinsModel,
           BYOLModel, NNCLRModel, NNSimSiamModel, NNBYOLModel]
 """
 
-model_names = ["MoCo_256"]
-models = [MocoModel]
+model_names = ["SimCLRModel"]
+models = [SimCLRModel]
 
 
 bench_results = []
