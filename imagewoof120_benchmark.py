8d1b1546809ddc064de252e3d75acb3091bd2336
Tiberio <tiberio-97@hotmail.it>
2022-06-22 19:59:44 +0200

    reduce batch size

--- imagewoof120_benchmark.py
+++ imagewoof120_benchmark.py
@@ -43,7 +43,7 @@ temperature=0.5
 warmup_epochs=0
 
 nmb_prototypes=120
-num_negatives=512
+num_negatives=256
 use_sinkhorn = True
 add_swav_loss = True
 false_negative_remove = False
@@ -75,7 +75,7 @@ nn_size=2 ** 16
 
 # benchmark
 n_runs = 1 # optional, increase to create multiple runs and report mean + std
-batch_sizes = [512]
+batch_sizes = [256]
 
 # use a GPU if available
 gpus = -1 if torch.cuda.is_available() else 0
