481bcd7b4f5492b78eb273b4be4866d2a471c166
Tiberio <tiberio-97@hotmail.it>
2022-07-04 17:04:46 +0200

    run moco original

--- imagewoof120_benchmark.py
+++ imagewoof120_benchmark.py
@@ -168,8 +168,8 @@ model_names = ['MoCo_256', 'SimCLR_256', 'SimSiam_256', 'BarlowTwins_256',
 models = [MocoModel, SimCLRModel, SimSiamModel, BarlowTwinsModel, 
           BYOLModel, NNCLRModel, NNSimSiamModel, NNBYOLModel]
 """
-model_names = ["NNN_Pos"]
-models = [NNNModel_Pos]
+model_names = ["MoCo_256"]
+models = [MocoModel]
 
 
 bench_results = []
