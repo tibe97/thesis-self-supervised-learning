af1fda16edaabbc807878a97dde396555802f9c2
Tiberio <tiberio-97@hotmail.it>
2022-07-08 08:54:53 +0200

    fix

--- imagewoof_linear_evaluator.py
+++ imagewoof_linear_evaluator.py
@@ -158,9 +158,9 @@ class MockupModel(pl.LightningModule):
             nn.Conv2d(last_conv_channels, num_ftrs, 1),
         )
         # create a simclr model based on ResNet
-        #self.resnet_simclr = lightly.models.MoCo(self.backbone, num_ftrs=num_ftrs, num_mlp_layers=2)
+        self.resnet_simclr = lightly.models.NNCLR(self.backbone, num_ftrs=num_ftrs, num_mlp_layers=2)
         #self.resnet_simclr = lightly.models.SimCLR(self.backbone, num_ftrs=num_ftrs)
-        self.resnet_simclr = lightly.models.NNCLR(self.backbone, num_ftrs=num_ftrs)
+        #self.resnet_simclr = lightly.models.NNCLR(self.backbone, num_ftrs=num_ftrs)
         self.criterion = lightly.loss.NTXentLoss()
 
         self.nn_replacer = NNMemoryBankModule(size=nn_size)
